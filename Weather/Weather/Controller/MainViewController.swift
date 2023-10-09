e5a913afcdbec34e2318283d801afca1acbe10f8
LeeZion94 <lion2212@naver.com>
2023-10-09 21:14:05 +0900

    feat: 프로젝트 기본 구성

---
+++ Weather/Weather/Controller/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/09.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        setUpConstratins()
+        setUpController()
+    }
+    
+    private func configureUI() {
+        
+    }
+    
+    private func setUpConstratins() {
+        
+    }
+    
+    private func setUpController() {
+        view.backgroundColor = .systemBackground
+    }
+}
+
