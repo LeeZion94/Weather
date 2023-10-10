f102022ee614f7fa2c42efd6397def6aa770c082
LeeZion94 <lion2212@naver.com>
2023-10-11 00:37:03 +0900

    feat: APIKeys, APIError Type 추가

---
+++ Weather/Weather/Networking/APIError.swift
@@ -0,0 +1,12 @@
+//
+//  APIError.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+import Foundation
+
+enum APIError: LocalizedError {
+    case error
+}
---
+++ Weather/Weather/Networking/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+struct APIKeys {
+    static var weather: String {
+        return "7fa59bcd481e6963e0b5c0e0f299c049"
+    }
+}
