16d97c6cc06d3c9880c2e504c5f5c35d07686425
Zion <lion2212@naver.com>
2023-10-11 13:09:41 +0900

    feat: URLSessionProvider 구현 및 3rd_party 추가 (Rx)

---
+++ Weather/Weather/Networking/URLSessionProvider.swift
@@ -0,0 +1,40 @@
+//
+//  URLSessionProvider.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol URLSessionProviderType {
+    func dataTask(url: URL?) -> Observable<Result<Data, APIError>>
+}
+
+final class URLSessionProvider: URLSessionProviderType {
+    private var dataTask: URLSessionDataTask?
+    
+    func dataTask(url: URL?) -> Observable<Result<Data, APIError>> {
+        guard let urlRequest = setUpUrlRequest(url: url) else {
+            return Observable.just(.failure(.error))
+        }
+        
+        return URLSession.shared.rx.data(request: urlRequest)
+            .map { data in
+                return .success(data)
+            }
+            .catch({ error in
+                return Observable.just(Result.failure(.error))
+            })
+            .asObservable()
+    }
+    
+    private func setUpUrlRequest(url: URL?) -> URLRequest? {
+        guard let url else { return nil }
+        var urlRequest = URLRequest(url: url)
+        
+        return urlRequest
+    }
+}
