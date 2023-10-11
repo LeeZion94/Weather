0bfeada5e2ea59880312af16db1c52aa9469fe7d
Zion <lion2212@naver.com>
2023-10-11 21:14:26 +0900

    feat: DetailWeatherDTO 추가

---
+++ Weather/Weather/Model/DTO/DetailWeatherDTO.swift
@@ -0,0 +1,15 @@
+//
+//  DetailWeatherDTO.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+import Foundation
+
+struct DetailWeatherDTO {
+    let leftTitle: String
+    let leftValue: String
+    let rightTitle: String
+    let rightValue: String
+}
