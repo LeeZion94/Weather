cd2d87e40df4df680597b06b9fbcd3e4f2efd440
Zion <lion2212@naver.com>
2023-10-11 21:40:06 +0900

    feat: HourlyWeatherViewController Layout 구현

---
+++ Weather/Weather/Controller/HourlyWeatherViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HourlyWeatherViewController.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+import UIKit
+
+final class HourlyWeatherViewController: UIViewController {
+    enum Section {
+        case main
+    }
+    
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private var diffableDataSource: UICollectionViewDiffableDataSource<Section, HourlyWeatherDTO>?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        setUpConstraints()
+        setUpDiffableDataSource()
+        setUPDiffableDataSourceSnapShot()
+    }
+    
+    private func configureUI() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setUpConstraints() {
+        
+    }
+}
+
+// MARK: - Diffable DataSource
+extension HourlyWeatherViewController {
+    private func setUpDiffableDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<HourlyWeatherCollectionViewCell,
+                                                                    HourlyWeatherDTO> { cell, indexPath, hourlyWeatherDTO in
+            cell.setUpContents(hourlyweatherDTO: hourlyWeatherDTO)
+        }
+        
+        diffableDataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, hourlyWeatherDTO in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                for: indexPath,
+                                                                item: hourlyWeatherDTO)
+        })
+    }
+    
+    private func setUPDiffableDataSourceSnapShot() {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, HourlyWeatherDTO>()
+        
+        snapShot.appendSections([.main])
+        snapShot.appendItems([]) // TODO : Required Data
+        diffableDataSource?.apply(snapShot)
+    }
+}
--- Weather/Weather/Model/DTO/HourlyWeatherDTO.swift
+++ Weather/Weather/Model/DTO/HourlyWeatherDTO.swift
@@ -5,7 +5,7 @@
 //  Created by Hyungmin Lee on 2023/10/11.
 //
 
-struct HourlyWeatherDTO {
+struct HourlyWeatherDTO: Hashable {
     let hour: String
     let imageName: String
     let temperature: String
