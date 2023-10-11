633f87b53812b9d7f6837c5901aa3e48fe9711b1
Zion <lion2212@naver.com>
2023-10-11 20:51:18 +0900

    feat: WeatherWeeklyTableViewCell 추가

---
+++ Weather/Weather/View/WeatherWeeklyTableViewCell.swift
@@ -0,0 +1,95 @@
+//
+//  WeatherWeeklyTableViewCell.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+import UIKit
+
+final class WeatherWeeklyTableViewCell: UITableViewCell, ReuseIdentifiable {
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 50
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let dayLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let weatherImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    private let temperatureStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private let maxTemperatureLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let minTemperatureLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        [maxTemperatureLabel, minTemperatureLabel].forEach {
+            temperatureStackView.addArrangedSubview($0)
+        }
+        
+        [dayLabel, weatherImageView, temperatureStackView].forEach {
+            mainStackView.addArrangedSubview($0)
+        }
+        
+        contentView.addSubview(mainStackView)
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            mainStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            mainStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5)
+        ])
+        
+        temperatureStackView.setContentHuggingPriority(.init(1), for: .horizontal)
+    }
+}
