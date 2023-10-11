6bc26e2e26deb3c39620eefd6e8af7777ab29dbe
Zion <lion2212@naver.com>
2023-10-11 15:47:03 +0900

    feat: Weather Model -> ForecastResult로 수정

---
+++ Weather/Weather/Model/ForecastResult.swift
@@ -0,0 +1,74 @@
+//
+//  ForecastResult.swift
+//  ForecastResult
+//
+//  Created by Hyungmin Lee on 2023/10/11.
+//
+
+struct ForecastResult: Decodable {
+    let cod: String
+    let message: Int
+    let cnt: Int
+    let list: [Forecast]
+    
+    struct Forecast: Decodable {
+        let dt: Int
+        let main: Main
+        let weather: [Weather]
+        let clouds: Clouds
+        let wind: Wind
+        let visibility: Int
+        let pop: Int
+        let sys: Sys
+        let dt_txt: String
+        
+        struct Main: Decodable {
+            let temp: Double
+            let feels_like: Double
+            let temp_min: Double
+            let temp_max: Double
+            let pressure: Int
+            let sea_level: Int
+            let grnd_level: Int
+            let humidity: Int
+            let temp_kf: Double
+        }
+        
+        struct Weather: Decodable {
+            let id: Int
+            let main: String
+            let description: String
+            let icon: String
+        }
+        
+        struct Clouds: Decodable {
+            let all: Int
+        }
+        
+        struct Wind: Decodable {
+            let speed: Int
+            let deg: Int
+            let gust: Double
+        }
+        
+        struct Sys: Decodable {
+            let pod: String
+        }
+    }
+    
+    struct City: Decodable {
+        let id: Int
+        let name: String
+        let coord: Coord
+        let country: String
+        let population: Int
+        let timezon: Int
+        let sunrise: Int
+        let sunset: Int
+        
+        struct Coord: Decodable {
+            let lat: Double
+            let lon: Double
+        }
+    }
+}
