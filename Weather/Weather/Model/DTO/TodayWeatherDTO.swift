1ebaad9b861fab900acad1e4e670c69365609e90
Zion <lion2212@naver.com>
2023-10-12 19:03:07 +0900

    feat: TodayWeatherDTO 추가

---
+++ Weather/Weather/Model/DTO/TodayWeatherDTO.swift
@@ -0,0 +1,12 @@
+//
+//  TodayWeatherDTO.swift
+//  Weather
+//
+//  Created by Hyungmin Lee on 2023/10/12.
+//
+
+struct TodayWeatherDTO {
+    let cityName: String
+    let weatherDescription: String
+    let temperature: String
+}
